b18ffbfe32b2de69b4357901ba7aeec44e053ce2
PrinceAtVidyaMantra <prince.kunwar@vidyamantra.com>
2022-07-25 18:02:35 +0530

    initial

---
+++ app.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <NuxtWelcome />
+  </div>
+</template>
+
+<script setup>
+// export default defineNuxtComponent({
+//   // async asyncData() {
+//   //   console.log("Aysnc data", process.client);
+//   //   return {};
+//   // },
+//   created() {
+//     console.log("Created", process.client);
+//   },
+// });
+
+useAsyncData(( ) => {
+  console.log("Aysnc data", process.client);
+  return {};
+})
+</script>
